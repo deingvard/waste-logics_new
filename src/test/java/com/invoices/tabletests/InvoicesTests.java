95935a3b4e13edbd80be0bd324893deb8dd8fb30
Ihar Iuliyeu <iiuliyeu@scnsoft.com>
2019-07-25 10:54:19 +0300

    Updated test documentation

--- src/test/java/com/invoices/tabletests/InvoicesTests.java
+++ src/test/java/com/invoices/tabletests/InvoicesTests.java
@@ -19,48 +19,48 @@ public class InvoicesTests extends TestUtilities {
         String orderIdText = invoicePage.getOrderIdText();
         // Verification that page contains expected text in source
         Assert.assertTrue(orderIdText.contains("146566"),
-                "Editor default text is not expected. It is: " + orderIdText);
+                "Invoices default text is not expected. It is: " + orderIdText);
 
         // Get default Invoice Address text
         String invoiceAddressText = invoicePage.getInvoiceAddress();
         // Verification that page contains expected text in source
         Assert.assertTrue(invoiceAddressText.contains("TEST ADDRESS, TEST TOWN, 111111"),
-                "Editor default text is not expected. It is: " + invoiceAddressText);
+                "Invoices default text is not expected. It is: " + invoiceAddressText);
 
         // Get default Grade text
         String gradeText = invoicePage.getGrade();
         // Verification that page contains expected text in source
         Assert.assertTrue(gradeText.contains("Mixed Municipal Waste"),
-                "Editor default text is not expected. It is: " + gradeText);
+                "Invoices default text is not expected. It is: " + gradeText);
 
         // Get default Weight text
         String weightText = invoicePage.getWeight();
         // Verification that page contains expected text in source
         Assert.assertTrue(weightText.contains("0.460 T"),
-                "Editor default text is not expected. It is: " + weightText);
+                "Invoices default text is not expected. It is: " + weightText);
 
         // Get default Company text
         String companyText = invoicePage.getCompany();
         // Verification that page contains expected text in source
         Assert.assertTrue(companyText.contains("TEST CUSTOMER"),
-                "Editor default text is not expected. It is: " + companyText);
+                "Invoices default text is not expected. It is: " + companyText);
 
         // Get default Price Entity Flat Charge text
         String priceEntityFlatChargeText = invoicePage.getPriceEntityFlatCharge();
         // Verification that page contains expected text in source
         Assert.assertTrue(priceEntityFlatChargeText.contains("£100.00"),
-                "Editor default text is not expected. It is: " + priceEntityFlatChargeText);
+                "Invoices default text is not expected. It is: " + priceEntityFlatChargeText);
 
         // Get default Price Entity Per Tonne text
         String priceEntityPerTonneText = invoicePage.getPriceEntityPerTonne();
         // Verification that page contains expected text in source
         Assert.assertTrue(priceEntityPerTonneText.contains("£4.60"),
-                "Editor default text is not expected. It is: " + priceEntityPerTonneText);
+                "Invoices default text is not expected. It is: " + priceEntityPerTonneText);
 
         // Get default Price Entity Item text
         String priceEntityItemText = invoicePage.getPriceEntityItem();
         // Verification that page contains expected text in source
         Assert.assertTrue(priceEntityItemText.contains("£110.10"),
-                "Editor default text is not expected. It is: " + priceEntityItemText);
+                "Invoices default text is not expected. It is: " + priceEntityItemText);
     }
 }
